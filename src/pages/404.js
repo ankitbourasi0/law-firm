95be44da33bc3a20c332e9e662caaafb2a365546
Ankit Bourasi <ankitbourasi0@gmail.com>
2023-11-02 19:07:11 +0530

    cover image condition

--- src/pages/404.js
+++ src/pages/404.js
@@ -38,7 +38,7 @@ export default function Custom404() {
         Uh-oh!
       </h1>
   
-      <p className="mt-4 text-gray-500">We can't find that page.</p>
+      <p className="mt-4 text-gray-500">{`We can\'t find that page.`}</p>
     </div>
   </div>
   }
